9579d1f92da194d5bf691eb84924df8cb8348c89
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-09 12:01:19 +0200

    diffusion class

--- sd/diffusion.py
+++ sd/diffusion.py
@@ -329,3 +329,51 @@ class UNET_OutputLayer(nn.Module):
 
         # (Batch_Size, 4, Height / 8, Width / 8)
         return x
+
+    class Diffusion(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.time_embedding = TimeEmbedding(320)
+            self.unet = UNET()
+            self.final = UNET_OutputLayer(320, 4)
+
+        def forward(self, latent, context, time):
+            # latent: (Batch_Size, 4, Height / 8, Width / 8)
+            # context: (Batch_Size, Seq_Len, Dim)
+            # time: (1, 320)
+
+            # (1, 320) -> (1, 1280)
+            time = self.time_embedding(time)
+
+            # (Batch, 4, Height / 8, Width / 8) -> (Batch, 320, Height / 8, Width / 8)
+            output = self.unet(latent, context, time)
+
+            # (Batch, 320, Height / 8, Width / 8) -> (Batch, 4, Height / 8, Width / 8)
+            output = self.final(output)
+
+            # (Batch, 4, Height / 8, Width / 8)
+            return output
+
+    class Diffusion(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.time_embedding = TimeEmbedding(320)
+            self.unet = UNET()
+            self.final = UNET_OutputLayer(320, 4)
+
+        def forward(self, latent, context, time):
+            # latent: (Batch_Size, 4, Height / 8, Width / 8)
+            # context: (Batch_Size, Seq_Len, Dim)
+            # time: (1, 320)
+
+            # (1, 320) -> (1, 1280)
+            time = self.time_embedding(time)
+
+            # (Batch, 4, Height / 8, Width / 8) -> (Batch, 320, Height / 8, Width / 8)
+            output = self.unet(latent, context, time)
+
+            # (Batch, 320, Height / 8, Width / 8) -> (Batch, 4, Height / 8, Width / 8)
+            output = self.final(output)
+
+            # (Batch, 4, Height / 8, Width / 8)
+            return output
