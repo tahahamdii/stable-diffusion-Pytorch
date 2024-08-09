00cf088b2d1c56291eefa68ba0eb208bc15a98a1
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-09 08:39:40 +0200

    batch size channel height and width

--- encoder.py
+++ encoder.py
@@ -8,6 +8,14 @@ class VAE_Encoder(nn.Sequential):
     def __init__(self):
         super().__init__(
 
-            nn.Conv2d(3,128,kernel_size=3,padding=1),
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0),
+            VAE_ResidualBlock(128, 256),
+            VAE_ResidualBlock(256, 256),
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=0),
+            VAE_ResidualBlock(256, 512),
+            VAE_ResidualBlock(512, 512)
 
         )
